b7120adc67acb636e63005493affa7ae2fe1e262
darkin02 <67747727+darkin02@users.noreply.github.com>
2022-06-06 17:17:01 +0700

    thuan commit

--- GiaoHangTietKiem/GiaoHangTietKiem/Controllers/BaseController.cs
+++ GiaoHangTietKiem/GiaoHangTietKiem/Controllers/BaseController.cs
@@ -11,12 +11,12 @@ namespace GiaoHangTietKiem.Controllers
         // GET: Base
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var sess = (UserLogin)Session[Common.Common.USER_SESSION];
-            if (sess == null)
-            {
-                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { Controller = "Login", action = "Login", Area = "Admin" }));
-            }
-            base.OnActionExecuting(filterContext);
+            //var sess = (UserLogin)Session[Common.Common.USER_SESSION];
+            //if (sess == null)
+            //{
+            //    filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { Controller = "Login", action = "Login", Area = "Admin" }));
+            //}
+            //base.OnActionExecuting(filterContext);
         }
     }
 }
